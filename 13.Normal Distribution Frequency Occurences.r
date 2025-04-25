f8b3b6a88a89f81582c54383a0938f19ffd0618b
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-04-25 17:03:35 +0300

    Delete 13.Normal Distribution Frequency Occurences.r

--- 13.Normal Distribution Frequency Occurences.r
+++
@@ -1,9 +0,0 @@
-# Generate 1000 random numbers from a normal distribution
-# with mean 50 and standard deviation 100, and round them
-# down to the nearest integer
-n = floor(rnorm(n=100, mean=50, sd=100))
-print(n)
-
-# Create a frequency table of the occurences of each unique value in 'n'
-t = table(n)
-print(t)
